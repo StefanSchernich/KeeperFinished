6a1bf96be77eac2ff5cc874760f0aae32e95900d
StefanSchernich <80632675+StefanSchernich@users.noreply.github.com>
2021-04-28 11:26:17 +0200

    Initial commit

---
+++ src/components/CreateArea.jsx
@@ -0,0 +1,72 @@
+import React, { useState } from "react";
+import AddIcon from "@material-ui/icons/Add";
+import Zoom from "@material-ui/core/Zoom";
+import Fab from "@material-ui/core/Fab";
+
+function CreateArea(props) {
+  const [note, setNote] = useState({
+    title: "",
+    content: ""
+  });
+
+  const [active, setActive] = useState(false);
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+
+    setNote((prevNote) => {
+      return {
+        ...prevNote,
+        [name]: value
+      };
+    });
+  }
+
+  function submitNote(event) {
+    props.onAdd(note);
+    setNote({
+      title: "",
+      content: ""
+    });
+    event.preventDefault();
+  }
+
+  return (
+    <div>
+      <form className="create-note">
+        {active && (
+          <input
+            name="title"
+            onChange={handleChange}
+            value={note.title}
+            placeholder="Title"
+          />
+        )}
+
+        <textarea
+          name="content"
+          onClick={() => setActive(true)}
+          onChange={handleChange}
+          value={note.content}
+          placeholder="Take a note..."
+          rows={active ? 3 : 1}
+        />
+        <Zoom in={active ? true : false}>
+          <Fab onClick={submitNote}>
+            <AddIcon />
+          </Fab>
+        </Zoom>
+      </form>
+    </div>
+  );
+}
+
+export default CreateArea;
+
+// Zum Start title ausgeblendet, nur content mit einer row sichtbar.
+// Button ausgeblendet
+
+// Bei Klick auf content -->
+// 3 Rows
+// Title einblenden
+// Button einblenden
